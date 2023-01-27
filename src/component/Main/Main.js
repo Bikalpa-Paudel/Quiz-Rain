6142350de6f90ba51b4c73b1d4ec7ca1a53806b7
Bikalpa <bikalpapaudel53@gmail.com>
2023-01-27 15:31:37 +0545

    adding main1 file for code structuring

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,33 +1,10 @@
-import{useState, useEffect} from "react"
 import "./Main.css"
+import Main1 from "./Main_1/Main1";
 export default function Main(){
-    const [data, setData] = useState([]);
-  
-    useEffect(() => {
-        fetch('https://opentdb.com/api.php?amount=5&category=9&type=multiple')
-          .then(response => response.json())
-          .then(data => setData(data.results))
-          .catch(error => console.log(error))
-      }, [])
 
-      const question = data.map(item => (
-        <div className="qna">
-            <h2 className="question">{item.question}</h2>
-            <div className="answers">
-                <p className="ans crt-ans">{item.correct_answer}</p>
-                {item.incorrect_answers.map(item1 => (
-                    <p className="ans">{item1}</p>
-                ))}
-            </div>
-            <div className="bottom-line"></div>
-
-        </div>
-    ))
     return(
     <div className="main-container">
-            <div>
-              {question}
-            </div>
+        <Main1 />
     </div>
     )
 }
---
+++ src/component/Main/Main_1/Main1.js
@@ -0,0 +1,33 @@
+import{useState, useEffect} from "react"
+import "./Main1.css"
+export default function Main1(){
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5&category=9&type=multiple')
+          .then(response => response.json())
+          .then(data => setData(data.results))
+          .catch(error => console.log(error))
+      }, [])
+
+      const question = data.map(item => (
+        <div className="qna">
+            <h2 className="question">{item.question}</h2>
+            <div className="answers">
+                <p className="ans crt-ans">{item.correct_answer}</p>
+                {item.incorrect_answers.map(item1 => (
+                    <p className="ans">{item1}</p>
+                ))}
+            </div>
+            <div className="bottom-line"></div>
+
+        </div>
+    ))
+    return (
+    <div className="main-container">
+        <div>
+          {question}
+        </div>
+    </div>
+    )
+}
