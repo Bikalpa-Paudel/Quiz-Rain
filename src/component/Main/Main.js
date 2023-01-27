353e1e9c2e8b99bc4679d6f7c20424c4d8fedc11
Bikalpa <bikalpapaudel53@gmail.com>
2023-01-27 13:52:27 +0545

    more commit

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,5 +1,5 @@
 import{useState, useEffect} from "react"
-
+import "./Main.css"
 export default function Main(){
     const [data, setData] = useState([]);
   
@@ -8,15 +8,26 @@ export default function Main(){
           .then(response => response.json())
           .then(data => setData(data.results))
           .catch(error => console.log(error))
-      }, []);
-      const question = data.map(item => 
-      [<p>{item.question}</p>,
-      <p>{item.correct_answer}</p>]
-      )
+      }, [])
+
+      const question = data.map(item => (
+        <div className="qna">
+            <h2 className="question">{item.question}</h2>
+            <div className="answers">
+                <p className="ans crt-ans">{item.correct_answer}</p>
+                {item.incorrect_answers.map(item1 => (
+                    <p className="ans">{item1}</p>
+                ))}
+            </div>
+            <div className="bottom-line"></div>
+
+        </div>
+    ))
     return(
-        <>
-            {question}
-            <h1>&quot; &shy;</h1>
-        </>
+    <div className="main-container">
+            <div>
+              {question}
+            </div>
+    </div>
     )
 }
