f51af035db68a5de3d695cb77cf51a835379b57b
Bikalpa <bikalpapaudel53@gmail.com>
2023-01-29 15:40:50 +0545

    extra commit

--- src/component/Main/Main_1/Main1.js
+++ src/component/Main/Main_1/Main1.js
@@ -2,6 +2,7 @@ import{useState, useEffect} from "react"
 import "./Main1.css"
 export default function Main1(){
     const [data, setData] = useState([]);
+    const [color, setColor] = useState('#F5F7FB')
   
     useEffect(() => {
         fetch('https://opentdb.com/api.php?amount=5&category=9&type=multiple')
@@ -10,16 +11,21 @@ export default function Main1(){
           .catch(error => console.log(error))
       }, [])
 
+
+
+    function changeColor(){
+        setColor(color === '#F5F7FB' ? "#D6DBF5" : '#F5F7FB')
+    }
       const question = data.map(item => (
         <div className="qna">
             <h2 className="question">{item.question}</h2>
             <div className="answers">
-                <p className="ans crt-ans">{item.correct_answer}</p>
                 {item.incorrect_answers.map(item1 => (
-                    <p className="ans">{item1}</p>
+                     <p style={{backgroundColor: color}} onClick={changeColor} className="ans">{item1}</p>
                 ))}
+                <p style={{backgroundColor: color}} onClick={changeColor}  className="ans crt-ans">{item.correct_answer}</p>
             </div>
-            <div className="bottom-line"></div>
+           <div className="bottom-line"></div>
 
         </div>
     ))
