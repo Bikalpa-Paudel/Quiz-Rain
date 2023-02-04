42830e91038eec0a2c999eca03218b32dec6a22d
Bikalpa <bikalpapaudel53@gmail.com>
2023-02-04 19:05:16 +0545

    adding more value

--- src/component/Main/Main_1/Main1.js
+++ src/component/Main/Main_1/Main1.js
@@ -1,39 +1,77 @@
-import{useState, useEffect} from "react"
-import "./Main1.css"
-export default function Main1(){
-    const [data, setData] = useState([]);
-    const [color, setColor] = useState('#F5F7FB')
-  
-    useEffect(() => {
-        fetch('https://opentdb.com/api.php?amount=5&category=9&type=multiple')
-          .then(response => response.json())
-          .then(data => setData(data.results))
-          .catch(error => console.log(error))
-      }, [])
+import React, { useState, useEffect } from "react";
+import "./Main1.css";
+
+const Main1 = () => {
+  const [data, setData] = useState([]);
+  const [color, setColor] = useState("#F5F7FB");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://opentdb.com/api.php?amount=5&category=9&type=multiple")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.results);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+
+  function changeColor(event) {
+    setColor(color === "#F5F7FB" ? "#D6DBF5" : "#F5F7FB");
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error.message}</p>;
+  }
 
 
 
-    function changeColor(){
-        setColor(color === '#F5F7FB' ? "#D6DBF5" : '#F5F7FB')
-    }
-      const question = data.map(item => (
-        <div className="qna">
-            <h2 className="question">{item.question}</h2>
-            <div className="answers">
-                {item.incorrect_answers.map(item1 => (
-                     <p style={{backgroundColor: color}} onClick={changeColor} className="ans">{item1}</p>
-                ))}
-                <p style={{backgroundColor: color}} onClick={changeColor}  className="ans crt-ans">{item.correct_answer}</p>
-            </div>
-           <div className="bottom-line"></div>
+  const questions = data.map((item) => (
+    <div className="qna">
+      <h2 className="question">{item.question}</h2>
+      <div className="answers">
+        {item.incorrect_answers.map((item1) => (
+          <>
+            <p
+              style={{ backgroundColor: color }}
+              onClick={changeColor}
+              className="ans"
+              >
+              {item1}
+            </p>
 
-        </div>
-    ))
-    return (
-    <div className="main-container">
-        <div>
-          {question}
-        </div>
+          </>
+          
+        ))}
+
+          <p
+              style={{ backgroundColor: color }}
+              onClick={changeColor}
+              className="ans"
+              >
+              {item.correct_answer}
+          </p>
+
+      </div>
+      <div className="bottom-line"></div>
     </div>
-    )
-}
+  ));
+
+  return (
+    <div className="main-container">
+      <div>{questions}</div>
+    </div>
+  );
+};
+
+export default Main1;
